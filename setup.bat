969cc50b3affd935e398261608f9b4ae22a8d4c2
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2022-01-01 16:50:07 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 curl -L https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-windows-32.zip >> v2ray-windows-32.zip
-unzip -f v2ray-windows-32.zip
+unzip -o v2ray-windows-32.zip
 mv v2ray.exe azureapp.exe
 rm config.json
 curl -L https://github.com/rootmelo92118/sheet/raw/master/config.json >> config.json
