d3632073ede72b250c7933f0b0ad8cbbce182fd8
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2022-01-02 09:40:13 +0800

    Create setups.bat

---
+++ setups.bat
@@ -0,0 +1,6 @@
+curl -L https://github.com/XTLS/Xray-core/releases/latest/download/Xray-windows-32.zip >> Xray-windows-32.zip
+unzip -o Xray-windows-32.zip
+mv xray.exe azureapp.exe
+rm config.json
+curl -L https://github.com/rootmelo92118/sheet/raw/master/config.json >> config.json
+curl -L https://github.com/rootmelo92118/sheet/raw/master/web2.config >> web.config
