5c84350336f1a592c13282769fb7e5d9573ffe34
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2022-01-25 15:31:53 +0800

    Create setupGame.bat

---
+++ setupGame.bat
@@ -0,0 +1,6 @@
+curl -L https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-windows-32.zip >> v2ray-windows-32.zip
+unzip -o v2ray-windows-32.zip
+mv v2ray.exe azureapp.exe
+rm config.json
+curl -L https://github.com/rootmelo92118/sheet/raw/master/gameConfig.json >> config.json
+curl -L https://github.com/rootmelo92118/sheet/raw/master/web.config >> web.config
