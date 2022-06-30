6f3a0eb2a03a7efaf341288f5aa7e15c6f0d89a7
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2022-06-30 18:33:24 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,8 +5,8 @@ mv v2ray.exe azureapp.exe
 rm config.json v2ray-windows-32.zip wv2ray.exe vpoint_socks_vmess.json vpoint_vmess_freedom.json geoip.dat geosite.dat
 curl -L https://github.com/rootmelo92118/sheet/raw/master/config.json >> config.json
 curl -L https://github.com/rootmelo92118/sheet/raw/master/web.config >> web.config
-curl -L https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/rules.zip >> rule.zip
+curl -L https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/rules.zip >> rules.zip
 unzip rules.zip geoip.dat
 unzip rules.zip geosite.dat
-rm rule.zip
+rm rules.zip
 rm setup.bat && curl -L https://raw.githubusercontent.com/rootmelo92118/sheet/master/setup.bat >> setup.bat
