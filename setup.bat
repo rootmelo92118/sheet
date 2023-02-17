ddfe9e63a8a9bce21d415f19ff3b9f3263d07c07
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-02-17 11:53:17 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
-rm config.json web.config v2ctl.exe azureapp.exe geoip.dat geosite.dat geoip-only-cn-private.dat
-curl -L https://github.com/v2fly/v2ray-core/releases/download/v4.45.2/v2ray-windows-32.zip >> v2ray-windows-32.zip
-unzip -o v2ray-windows-32.zip
-mv v2ray.exe azureapp.exe
-rm config.json v2ray-windows-32.zip wv2ray.exe vpoint_socks_vmess.json vpoint_vmess_freedom.json geoip.dat geosite.dat
+rm config.json web.config azureapp.exe geoip.dat geosite.dat geoip-only-cn-private.dat
+curl -L https://github.com/XTLS/Xray-core/releases/latest/download/Xray-windows-32.zip >> Xray-windows-32.zip
+unzip -o Xray-windows-32.zip
+mv xray.exe azureapp.exe
+rm Xray-windows-32.zip wxray.exe README.md LICENSE geoip.dat geosite.dat
 curl -L https://github.com/rootmelo92118/sheet/raw/master/config.json >> config.json
 curl -L https://github.com/rootmelo92118/sheet/raw/master/web.config >> web.config
 curl -L https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/rules.zip >> rules.zip
